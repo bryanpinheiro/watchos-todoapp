5963fd7ba3a85448f81478e67f0469abd51c74ed
Bryan Pinheiro <bryansouza_01@outlook.com>
2023-10-08 22:01:19 -0300

    Initial Commit

---
+++ watchos-todoapp Watch App/watchos_todoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchos_todoappApp.swift
+//  watchos-todoapp Watch App
+//
+//  Created by Bryan on 08/10/23.
+//
+
+import SwiftUI
+
+@main
+struct watchos_todoapp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
